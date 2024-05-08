0185a400aa765ce882ba38e035c8a3f75ea3b812
gustav123ze <guze22ab@student.cbs.dk>
2024-05-08 12:11:13 +0200

    ret

--- server.js
+++ server.js
@@ -1,7 +1,48 @@
+//root/server.js
+
 const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+
+
+
+//SQLite
+const sqlite3 = require('sqlite3').verbose();
+
+// open the database
+let db = new sqlite3.Database('./db/chinook.db', sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the chinook database.');
+});
+
+db.serialize(() => {
+  db.each(`SELECT PlaylistId as id,
+                  Name as name
+           FROM playlists`, (err, row) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log(row.id + "\t" + row.name);
+  });
+});
+
+db.close((err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Close the database connection.');
+});
+
+
+
+
+
+
+
+
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,21 +53,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Sti til backend-ruter
-app.use('/api', require('./server/routes/orders'));
-
+app.use('/api/orders', require('./server/routes/orders')); // Opretter ruten /api/orders
 
 // Startside route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
-
-
-
-
+    res.sendFile(path.join(__dirname, 'public', 'index1.html'));
+});
 
 // Start serveren
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Serveren kører på http://localhost:${port}`);
+    console.log(`Serveren kører på http://localhost:${port}`);
 });
 
