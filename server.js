babd7c087c09c3a41944da60b43452d5e11904c8
gustav123ze <guze22ab@student.cbs.dk>
2024-05-08 09:26:41 +0200

    ret

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+
+
+
+
 // Definerer en simpel rute
 app.get('/', (req, res) => {
   res.send('Hej verden!');
 });
+
+
+
+
+
+
 
 // Starter serveren
 app.listen(port, () => {
